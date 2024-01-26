4e9fc297aa163e13529cfa75aa0f3de600824746
mcharouk <marc.charouk.ext@allianz-trade.com>
2024-01-26 08:28:09 +0100

    chnage paths

---
+++ Demos/Module_10/TransitGateway/cleanResources.py
@@ -0,0 +1,71 @@
+# select all transit gateway route tables
+
+import time
+
+import boto3
+
+ec2 = boto3.client("ec2")
+
+
+def get_transit_gateway_route_tables():
+    response = ec2.describe_transit_gateway_route_tables()
+    return response["TransitGatewayRouteTables"]
+
+
+route_tables = get_transit_gateway_route_tables()
+
+default_route_table = None
+custom_route_table = None
+
+for route_table in route_tables:
+    if route_table["DefaultAssociationRouteTable"] == True:
+        default_route_table = route_table
+    else:
+        custom_route_table = route_table
+
+if custom_route_table is not None:
+    # disassociate all associations
+    custom_route_table_id = custom_route_table["TransitGatewayRouteTableId"]
+    associations = ec2.get_transit_gateway_route_table_associations(
+        TransitGatewayRouteTableId=custom_route_table_id
+    )["Associations"]
+
+    attachments = []
+    for association in associations:
+        attachmentId = association["TransitGatewayAttachmentId"]
+        ec2.disassociate_transit_gateway_route_table(
+            TransitGatewayRouteTableId=custom_route_table_id,
+            TransitGatewayAttachmentId=attachmentId,
+        )
+        attachments.append(attachmentId)
+
+    attachmentDisassosiationCompleted = False
+    while attachmentDisassosiationCompleted == False:
+        print("checking for associations")
+        associations = ec2.get_transit_gateway_route_table_associations(
+            TransitGatewayRouteTableId=custom_route_table_id
+        )["Associations"]
+
+        attachmentDisassosiationCompleted = True
+        for association in associations:
+            if association["State"] != "disassociated":
+                attachmentDisassosiationCompleted = False
+                break
+
+        if attachmentDisassosiationCompleted == False:
+            print("waiting for attachments to disassociate")
+            time.sleep(5)
+
+    print("attaching to default route table")
+    for attachment_id in attachments:
+        ec2.associate_transit_gateway_route_table(
+            TransitGatewayRouteTableId=default_route_table[
+                "TransitGatewayRouteTableId"
+            ],
+            TransitGatewayAttachmentId=attachment_id,
+        )
+
+    print("deleting custom route table")
+    ec2.delete_transit_gateway_route_table(
+        TransitGatewayRouteTableId=custom_route_table_id
+    )
---
+++ Demos/Module_9/Fargate/cleanResources.py
@@ -0,0 +1,36 @@
+# delete all ecs services
+
+import boto3
+
+client = boto3.client("ecs")
+
+
+# delete all ecs clusters
+response = client.list_clusters()
+for cluster in response["clusterArns"]:
+    response = client.list_services(cluster=cluster)
+    for service in response["serviceArns"]:
+        print("deleting " + service)
+        client.delete_service(cluster=cluster, service=service, force=True)
+    print("deleting " + cluster)
+    client.delete_cluster(cluster=cluster)
+
+
+# delete all ecs task definitions
+response = client.list_task_definitions()
+for task in response["taskDefinitionArns"]:
+    print("deleting " + task)
+    client.deregister_task_definition(taskDefinition=task)
+
+
+# delete all ecr repositories
+
+import boto3
+
+client = boto3.client("ecr")
+
+response = client.describe_repositories()
+
+for repo in response["repositories"]:
+    print("deleting " + repo["repositoryName"])
+    client.delete_repository(repositoryName=repo["repositoryName"], force=True)
