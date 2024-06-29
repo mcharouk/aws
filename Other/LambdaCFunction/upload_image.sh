a1bf66ed3cf2c8ecf52c1ec0a48997cf16b41252
mcharouk <marc.charouk.ext@allianz-trade.com>
2024-06-29 22:53:33 +0200

    update

--- Other/LambdaCFunction/upload_image.sh
+++ Other/LambdaCFunction/upload_image.sh
@@ -2,11 +2,10 @@
 
 image_name=lambda-c-function
 region_name=eu-west-3
-repository_name=637423642269.dkr.ecr.eu-west-3.amazonaws.com
+account_id=637423642269
+repository_name=$account_id.dkr.ecr.$region_name.amazonaws.com
 
 aws ecr get-login-password --region $region_name | docker login --username AWS --password-stdin $repository_name
 docker build --platform linux/amd64 -t $image_name:test .
 docker tag $image_name:test $repository_name/$image_name:latest
 docker push $repository_name/$image_name:latest
-
-# docker run --platform linux/amd64 -p 9000:8080 lambda-c-function:test
