1e22820185876af996ce673d7406e2b6620c3a27
mcharouk <marc.charouk.ext@allianz-trade.com>
2024-08-13 14:01:34 +0200

    update

--- Demos/TechEssentials/Module_2/EC2/module_2/module_2_stack.py
+++ Demos/TechEssentials/Module_2/EC2/module_2/module_2_stack.py
@@ -14,7 +14,9 @@ class Module2Stack(Stack):
 
         vpc_name = "ComputeDemo"
         private_subnet = ec2.SubnetConfiguration(
-            name="Private", subnet_type=ec2.SubnetType.PRIVATE_ISOLATED
+            name="Private",
+            subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
+            map_public_ip_on_launch=True,
         )
         vpc = ec2.Vpc(
             self,
