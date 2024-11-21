2c9c3a1059c89abcd9d1cc6215e46bcf64290b01
mcharouk <marc.charouk.ext@allianz-trade.com>
2024-11-21 16:48:56 +0100

    update

--- Demos/cleanAllResources.py
+++ Demos/cleanAllResources.py
@@ -42,13 +42,16 @@ def purge_cloudwatch_log_groups(region_name):
     logs = boto3.client("logs", region_name=region_name)
     # list all cloudwatch log groups
     log_groups = logs.describe_log_groups()
-
+    log_group_to_exclude = [
+        "logs_demo",
+        "/aws/lambda/aws-controltower-NotificationForwarder",
+    ]
     if "logGroups" in log_groups:
         if len(log_groups["logGroups"]) == 0:
             print("No log groups to delete in region {0}".format(region_name))
         for group in log_groups["logGroups"]:
             log_group_name = group["logGroupName"]
-            if log_group_name == "logs_demo":
+            if log_group_name in log_group_to_exclude:
                 print(f"skipping log group {log_group_name}")
                 continue
             print(f"Deleting {group['logGroupName']} in region {region_name}")
